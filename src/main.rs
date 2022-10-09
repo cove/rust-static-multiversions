d63689582b47fa479f2a1e0e09a41612e84a9183
cove <covecs@gmail.com>
2022-10-09 10:36:45 -0700

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 
 fn main() {
     println!("I'm version v{}", lib::version());
-    println!("I'm version v{}", lib::version());
-    println!("I'm version v{}", lib::version());
-    println!("I'm version v{}", lib::version());
-
+    println!("I'm version v{}", lib_compat1::version());
 }
