841a1e47071922fb2bb2d70cd993d9f41ca15817
cove <covecs@gmail.com>
2022-10-09 10:51:52 -0700

    add v3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 fn main() {
-    println!("I'm version v{}", lib::version());
-    println!("I'm version v{}", lib_compat1::version());
+    println!("I'm the current version v{}", lib::version());
+    println!("I'm compat1 version v{}", lib_compat1::version());
+    println!("I'm compat2 version v{}", lib_compat2::version());
 }
